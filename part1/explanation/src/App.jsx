2a063c15340b8b3a30b3b7b3c5add3e3bbbb1e65
Yoana Dimitrova Penkova <yoanapenkova@outlook.com>
2024-08-09 16:42:15 +0200

    part1: explanation in d

--- part1/explanation/src/App.jsx
+++ part1/explanation/src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from 'react'
 
 const Display = ({ counter }) => <div>{counter}</div>
 
+/* DESTRUCTURED FORM
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+*/
+
+/*  STATE WITH A SINGLE VALUE
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
@@ -27,6 +31,98 @@ const App = () => {
         onClick={decreaseByOne}
         text='minus'
       />  
+    </div>
+  )
+}
+
+*/
+
+/*  STATE WITH MORE THAN ONE VALUE 
+
+const App = () => {
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () =>
+    setClicks({ ...clicks, left: clicks.left + 1 })
+  
+  const handleRightClick = () =>
+    setClicks({ ...clicks, right: clicks.right + 1 })
+
+  return (
+    <div>
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
+    </div>
+  )
+}
+
+*/
+
+/* ARRAYS */
+
+/* WITH CONDITIONAL RENDERING */
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+/* WITHOUT DESTRUCTURING */
+
+const Button = (props) => { 
+  console.log(props)
+  const { onClick, text } = props
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const [total, setTotal] = useState(0)
+
+  //handlers are often defined in a separate place
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right) 
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  }
+
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text='left' /> 
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
