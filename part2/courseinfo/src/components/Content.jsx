df4cc7f894d223f51d6ef1a8d5c4a9c1a0603d56
Yoana Dimitrova Penkova <yoanapenkova@outlook.com>
2024-08-10 19:07:43 +0200

    part2: exercise 2.2 done

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,11 +1,21 @@
 import Part from './Part'
 
 const Content = ({content}) => {
+
+    function totalExtercises(parts) {
+        let total = 0
+        for (let i = 0; i < parts.length; i++) {
+            total += parts[i].exercises
+        }
+        return total
+    }
+
     return (
       <div>
         {content.map(part => 
           <Part key={part.id} part={part} />
         )}
+        <b>total of {totalExtercises(content)} exercises</b>
       </div>
     )
   }
