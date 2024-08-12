a0c5aca63396f401bfb4e4f4a978799246354ec1
Yoana Dimitrova Penkova <yoanapenkova@outlook.com>
2024-08-12 18:22:07 +0200

    part2: explanation in c

--- part2/explanation/src/App.jsx
+++ part2/explanation/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
 const App = (props) => {
@@ -31,7 +32,7 @@ const App = (props) => {
   )
   */
 
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(
     'a new note...'
   )
@@ -59,6 +60,17 @@ const App = (props) => {
     : notes.filter(note => note.important)
 
   /*NOTE AS A SEPARATE COMPONENT*/
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
   return (
     <div>
